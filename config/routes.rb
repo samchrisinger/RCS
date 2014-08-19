bee7af1720ecc872eb8977d73370994738242ccc
Sam Chrisinger <shc7pw@virginia.edu>
2014-08-19 14:47:20 -0600

    Resolving git conflict in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-RCS::Application.routes.draw do
-  match '*path', :controller => 'application', :action => 'handle_options_request', :constraints => {:method => 'OPTIONS'}
-
-  devise_for :users
-=======
 RCS::Application.routes.draw do  
   match '*path', :controller => 'application', :action => 'handle_options_request', :constraints => {:method => 'OPTIONS'}
->>>>>>> 42fbcc89cc5f5b1e0755e87d0a7d41f27ea205fc
 
   devise_for :users do post '/users' => 'users#create' end 
   
